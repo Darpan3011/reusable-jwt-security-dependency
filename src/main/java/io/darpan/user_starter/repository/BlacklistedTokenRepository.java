6dd372f681054aabbe542e14789251862cc9b159
darpan kanani <darpan.k@dashtechinc.com>
2025-10-15 11:21:45 +0530

    added @Transactional for tokens

--- src/main/java/io/darpan/user_starter/repository/BlacklistedTokenRepository.java
+++ src/main/java/io/darpan/user_starter/repository/BlacklistedTokenRepository.java
@@ -1,7 +1,9 @@
 package io.darpan.user_starter.repository;
 
 import io.darpan.user_starter.model.BlacklistedToken;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.time.Instant;
@@ -10,6 +12,8 @@ import java.util.Optional;
 public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, Long> {
     Optional<BlacklistedToken> findByToken(String token);
 
+    @Transactional
+    @Modifying
     @Query("DELETE FROM BlacklistedToken t WHERE t.expiryDate < :now")
     void deleteExpiredTokens(@Param("now") Instant now);
 }
--- src/main/java/io/darpan/user_starter/repository/UserV2Repository.java
+++ src/main/java/io/darpan/user_starter/repository/UserV2Repository.java
@@ -2,11 +2,12 @@ package io.darpan.user_starter.repository;
 
 import io.darpan.user_starter.model.UserV2;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserV2Repository extends JpaRepository<UserV2, Long> {
+public interface UserV2Repository extends JpaRepository<UserV2, Long>, JpaSpecificationExecutor<UserV2> {
     Optional<UserV2> findByUsername(String username);
 }
